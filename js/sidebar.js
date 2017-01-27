f68bfc93100cafba2dcbdd23a8de6b5a30b9f59a
Steven Roberts <sarj21@vt.edu>
2017-01-26 21:06:25 -0500

    Preloader bug fix, sidebar.js waits til ready

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,31 +1,32 @@
-var setLogo = function() {
-    $('.secondary-logo').each(function() {
-        var shapeId = '#' + $(this).attr("data-target");
-        $(this).css('top',
-            $(shapeId).offset().top -
-            $(this).closest('.section').offset().top
-        );
-    });
-};
-
 $(document).scroll(function() {
-    setLogo();
+  $('.secondary-logo').each(function() {
+      var shapeId = '#' + $(this).attr("data-target");
+      $(this).css('top',
+          $(shapeId).offset().top -
+          $(this).closest('.section').offset().top
+      );
+  });
 });
 
-$('.links').mouseover(function() {
-    var hoveredLink = $(this).attr('href').substring(1);
-    $('.menu-container').each(function() {
-        if ($(this).attr('data-target') == hoveredLink) {
-            $(this).find("span").css('opacity', 1);
-        }
-    });
-});
+$(document).ready(function(){
 
-$('.links').mouseout(function() {
-    var hoveredLink = $(this).attr('href').substring(1);
-    $('.menu-container').each(function() {
-        if ($(this).attr('data-target') == hoveredLink) {
-            $(this).find("span").css('opacity', 0);
-        }
-    });
+  $(".links").mouseover(function() {
+      var hoveredLink = $(this).attr('href').substring(1);
+      console.log(hoveredLink);
+      $('.menu-container').each(function() {
+          if ($(this).attr('data-target') == hoveredLink) {
+              $(this).find("span").css('opacity', 1);
+          }
+      });
+  });
+
+  $(".links").mouseout(function() {
+      var hoveredLink = $(this).attr('href').substring(1);
+      $('.menu-container').each(function() {
+          if ($(this).attr('data-target') == hoveredLink) {
+              $(this).find("span").css('opacity', 0);
+          }
+      });
+  });
+
 });
